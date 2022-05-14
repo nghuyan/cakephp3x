9b09936b53ee953a72ff862f01fc2ba21a1f51fd
annh <nghuyan@gmail.com>
2022-05-14 09:46:07 +0700

    article example

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,41 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
+
+// Add the following method.
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmptyString('title', 'Title cannot be empty', false)
+            ->minLength('title', 10)
+            ->maxLength('title', 255);
+
+        $validator
+            ->allowEmptyString('body', 'Body cannot be empty', false)
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+}
